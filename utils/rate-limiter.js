92df21b5ee30512273d0171fbe8847c8d2a550e4
Nitzan Smulevici <93406243+nitzanye@users.noreply.github.com>
2023-04-06 22:13:04 +0300

    Update rate-limiter.js

--- utils/rate-limiter.js
+++ utils/rate-limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // in 15 minutes
-  max: 100, // you can make a maximum of 100 requests from one IP
+  max: 100, // max requests from one IP
 });
 
 module.exports = limiter;
