33a7eaff2b20c792daf3f0c5404bdb3e6ac78aa0
Nitzan Smulevici <93406243+nitzanye@users.noreply.github.com>
2023-04-06 22:11:19 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,7 +6,6 @@ const UnauthorizedError = require('../errors/unauthorized-error');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    // required: true,
     minlength: 2,
     maxlength: 30,
     default: 'Nitzan',
