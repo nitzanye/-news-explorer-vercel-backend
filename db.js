baa71fb441d6a93884715232f9db0a0a131ace13
NitzanSmulevici <nitzanye@gmail.com>
2023-04-07 17:19:54 +0300

    change db name

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const dbName = "newsdata";
+const dbName = "NEW-EXPLORER-DB";
 const password = process.env.MONGO_ATLAS_PASSWORD;
 
 const uri = `mongodb+srv://ADMIN:${password}@cluster0.5ksyjkb.mongodb.net/${dbName}retryWrites=true&w=majority`;
