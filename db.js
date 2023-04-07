a0dfe01fa4412712459ed52afb8c9de5a4ec6c2c
NitzanSmulevici <nitzanye@gmail.com>
2023-04-07 17:15:03 +0300

    add mongo atlas db

---
+++ db.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+const dbName = "newsdata";
+const password = process.env.MONGO_ATLAS_PASSWORD;
+
+const uri = `mongodb+srv://ADMIN:${password}@cluster0.5ksyjkb.mongodb.net/${dbName}retryWrites=true&w=majority`;
+
+mongoose.connect(uri);
--- utils/configuration.js
+++ utils/configuration.js
@@ -1,7 +1,7 @@
-const database = 'mongodb://localhost:27017/news-explorer';
-const privateKey = 'some-secret-key';
+// const database = 'mongodb://localhost:27017/news-explorer';
+// const privateKey = 'some-secret-key';
 
-module.exports = {
-  database,
-  privateKey,
-};
+// module.exports = {
+//   database,
+//   privateKey,
+// };
