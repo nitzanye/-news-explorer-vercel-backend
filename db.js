a04beaa8b29db87ec74e3df28deb5e82cf9b8df3
NitzanSmulevici <nitzanye@gmail.com>
2023-04-07 18:16:18 +0300

    delete commen

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+
 const mongoose = require("mongoose");
+
 const dbName = "test";
 const password = process.env.MONGO_ATLAS_PASSWORD;
 const uri = `mongodb+srv://ADMIN:${password}@cluster0.5ksyjkb.mongodb.net/${dbName}`;
