8b9add190fad7b0f99e1b9d9b5fc60d2a137a629
NitzanSmulevici <nitzanye@gmail.com>
2023-04-09 16:29:28 +0300

    delete comments

--- routes/signin.js
+++ routes/signin.js
@@ -5,7 +5,6 @@ const router = express.Router();
 const { login } = require("../controllers/users");
 const { validateLogin } = require("../middlewares/validations");
 
-// login
 // checks the email and password passed in the body and returns a JWT
 router.post("/", validateLogin, login);
 
